a3c33be0a0b773f95f636649c3d5d0f9cea82c3e
Midori-Gift <okometaberoyo5009@gmail.com>
2021-09-30 18:56:57 +0900

    2章修了

--- chapter-2/2-8/src/StrongBox.java
+++ chapter-2/2-8/src/StrongBox.java
@@ -3,4 +3,9 @@ public class StrongBox<E> {
 	private E data;
 	public void put(E d) {this.data = d;}
 	public E get() {return this.data;}
+	private KeyType keyType;
+	private 
+	public StrongBox(KeyType keyType) {
+		System.out.println(keyType);
+	}
 }
